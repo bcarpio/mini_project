7546b0bae3c3818b172cc0e14ed6406f46c33032
Brian Carpio <bcarpio@thetek.net>
2016-12-30 17:54:55 -0700

    added another serverspec test

--- playbook/serverspec/roles/apache/apache_spec.rb
+++ playbook/serverspec/roles/apache/apache_spec.rb
@@ -3,3 +3,7 @@ require_relative '../../spec_helper'
 describe package('apache2') do
 	  it { should be_installed }
 end
+
+describe file ('/var/www/html/index.html') do
+	it { should contain 'Automation for the People' }
+end
